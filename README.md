903d48d40ceaa5f762a54c847c78506d7c24d1f4
Michael XU <michaelxu@Michaels-MacBook-Pro.local>
2019-07-05 19:40:40 -0500

    Complete the Variable Rules, and add more in README.md

--- README.md
+++ README.md
@@ -4,3 +4,11 @@
 
 ### For quickstart application
 The reference page is http://flask.pocoo.org/docs/1.0/quickstart/#accessing-request-data
+
+#### Convert types:
+|'string'|(default) accepts any text without a slash|
+|'int'|accepts positive integers|
+|'float'|accepts positive floating point values|
+|'path'|like 'string' but also accepts slash|
+|'uuid'|accepts UUID strings|
+
--- quickstart/hello.py
+++ quickstart/hello.py
@@ -8,3 +8,18 @@ def index():
 @app.route('/hello')
 def hello():
     return 'Hello, World!'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return "User %s" % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # Show the post with given ID, the ID is an integer
+    return "Post %d" % post_id
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # Show the subpath after '/path'
+    return 'Subpath is %s' % subpath
